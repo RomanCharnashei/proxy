7b2d619376d4a2e9effd1dc123869978f51b3235
Charnashei, Roman <Roman.Charnashei@kronos.com>
2017-02-11 17:10:47 +0300

    refactoring

--- html5/setup-main.js
+++ html5/setup-main.js
@@ -3,13 +3,12 @@ let chalk = require('chalk');
 let config = require("../config");
 let router = express.Router();
 
-router.use(middleware, express.static(config.staticPath));
-
-function middleware(req, res, next) {
+router.use(function (req, res, next){
     req.url = '/setup-main' + req.url.replace('main.min.js', 'main.js');
     console.log(`${chalk.green('Redirect to: ')}${req.url}`);
     next();
-};
+});
 
+router.use(express.static(config.staticPath));
 
 module.exports = router;
